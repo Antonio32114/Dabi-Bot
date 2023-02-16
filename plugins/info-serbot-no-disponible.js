62bd5ec4fe846c7a0ea619bb6edff57f17d766af
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-02-16 11:59:26 -0300

    Add files via upload

---
+++ plugins/info-serbot-no-disponible.js
@@ -0,0 +1,32 @@
+import fs from 'fs'
+//import fetch from 'node-fetch'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+//let pp = 'https://i.imgur.com/WwxBm8o.jpg' //'./media/menus/Menu1.jpg'
+//let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+//try {
+//pp = await conn.getProfilePicture(who)
+//} catch (e) {
+
+//} finally { }
+  await m.reply(`╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮`)
+  //conn.sendButton(m.chat, str, author, await(await fetch(pp)).buffer(), [['Menu', '/menu']], m)
+let text = `
+este comando todavía no esta terminado el este bot`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '.',
+body: 'Super Bot WhatsApp',         
+previewType: 1, thumbnail: fs.readFileSync("./media/Menu2.jpg"),
+sourceUrl: `${yt}`}}})  
+  
+  let info = `*Ya que no esta disponible el #serbot, Ahora podrás unir el Número de este Bot a tú Grupo de manera Temporal usando el comando*\n*#join*
+  `.trim()
+  
+  await conn.sendHydrated(m.chat, info, wm, null, yt, 'y', null, null, [
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)
+  
+  //await conn.sendButton(m.chat, `Infórmate sobre las Novedades y recuerda tener la última versión.\nFind out about what's new and remember to have the latest version.`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', /${command}], ['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 2 🔄', /${command}]], m)
+}
+handler.command = /^(serbot|jadibot)/i
+export default handler
