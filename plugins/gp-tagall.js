82655815c2a2a47b330c42a8a53955491520eee9
Antonio <110497758+Antonio32114@users.noreply.github.com>
2023-02-17 16:36:40 -0500

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`╔══✪〘 *𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾* 〙✪══\n╠➥ɢʀᴜᴘᴏs : *${groupMetadata.subject}*\n╠➥ ᴍɪᴇᴍʙʀᴏs : *${participants.length}*${text ? `\n╠\n╠➥ Mensaje : ${text}\n` : ''}\n╠\n╠➥ ᴇᴛɪǫᴜᴇᴛᴀs\n` + users.map(v => '╠🔸️ @' + v.replace(/@.+/, '')).join`\n` + '\n╚═〘 *NovaBot-MD* 〙\n\n▌│█║▌║▌║║▌║▌║▌║█', null, {
+    m.reply(`╔══✪〘 *𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾* 〙✪══\n╠➥ɢʀᴜᴘᴏs : *${groupMetadata.subject}*\n╠➥ ᴍɪᴇᴍʙʀᴏs : *${participants.length}*${text ? `\n╠\n╠➥ Mensaje : ${text}\n` : ''}\n╠\n╠➥ ᴇᴛɪǫᴜᴇᴛᴀs\n` + users.map(v => '╠🔸️ @' + v.replace(/@.+/, '')).join`\n` + '\n╚═〘 *DabiBot-MD* 〙\n\n▌│█║▌║▌║║▌║▌║▌║█', null, {
         mentions: users
     })
 }
