e44aab8876300a4726beba5a55116cdc490fb72b
Antonio <110497758+Antonio32114@users.noreply.github.com>
2023-03-01 18:48:28 -0500

    Update config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -222,6 +222,4 @@ handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['nable']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
 
-handler.register = true
-
 export default handler
