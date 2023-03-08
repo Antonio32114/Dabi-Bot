3eed274c04c5e45cb0a53f0d73a086e79e6bce4d
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-08 18:34:56 -0300

    Add files via upload

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -1,38 +1,25 @@
-
-//import db from '../lib/database.js'
-
-const free = 5000
-const prem = 20000
-
-let handler = async (m, {conn, isPrems }) => {
-  let time = global.db.data.users[m.sender].lastclaim + 86400000
-  if (new Date - global.db.data.users[m.sender].lastclaim < 86400000) throw `🎁 *Ya recogiste tu recompensa diaria*\n\n🕚 Vuelve en *${msToTime(time - new Date())}* `
-  global.db.data.users[m.sender].exp += isPrems ? prem : free
-  m.reply(`
-🎁 *RECOMPENSA DIARIA*
-
-▢ *Has recibido:*
-🆙 *XP* : +${isPrems ? prem : free}`)
-  global.db.data.users[m.sender].lastclaim = new Date * 1
+const rewards = {
+  exp: 9999,
+  money: 4999,
+  potion: 5,
 }
-handler.help = ['daily']
-handler.tags = ['econ']
-handler.command = ['daily', 'claim'] 
+const cooldown = 86400000
+let handler = async (m,{ conn} ) => {
+  let user = global.db.data.users[m.sender]
+  if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily claim!, wait for *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
+  let text = ''
+  for (let reward of Object.keys(rewards)) {
+    if (!(reward in user)) continue
+    user[reward] += rewards[reward]
+    text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
+  }
+  conn.sendButton(m.chat,'*––––––『 DAILY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Weekly', '.weekly']],m)
+  user.lastclaim = new Date * 1
+}
+handler.help = ['daily', 'claim']
+handler.tags = ['xp']
+handler.command = /^(daily|claim)$/i
+
+handler.cooldown = cooldown
 
 export default handler
-
-
-
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " Horas " + minutes + " Minutos"
-}
-
