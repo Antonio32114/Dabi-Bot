a4d4dffff1ccc5554db6445d962bdd8ad2501cbe
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-03-09 15:48:52 -0300

    Update rpg-weekly.js

--- plugins/rpg-weekly.js
+++ plugins/rpg-weekly.js
@@ -13,7 +13,7 @@ let handler = async (m) => {
     user[reward] += rewards[reward]
     text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
   }
-  conn.sendButton(m.chat,'*––––––『 WEEKLY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Monthly', '.monthly']],m)
+  conn.sendButton(m.chat,'*––––––『 REGLAMO WEEKLY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Monthly', '.monthly']],m)
   user.lastweekly = new Date * 1
 }
 handler.help = ['weekly']
