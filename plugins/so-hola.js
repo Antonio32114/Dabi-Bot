c75e2c03bdcc57be16d96cc3f4816eaa45dae8e6
Antonio <110497758+Antonio32114@users.noreply.github.com>
2023-03-11 14:15:56 -0500

    Create so-hola.js

---
+++ plugins/so-hola.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Hola.mp3'
+conn.sendFile(m.chat, vn, 'Hola.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.command = /^(hola|ola|Hola🤖|ola🤖)$/i
+
+module.exports = handler
